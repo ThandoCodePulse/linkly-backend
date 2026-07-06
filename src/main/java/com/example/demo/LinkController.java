15a8ad0598e817e5d5fb089b2d75af12509f013d
ThandoCodePulse <thandolwenkosilindelwab@gmail.com>
2026-07-06 12:38:13 +0200

    Add random short code generation

--- src/main/java/com/example/demo/LinkController.java
+++ src/main/java/com/example/demo/LinkController.java
@@ -1,5 +1,8 @@
 package com.example.demo;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Random;
+
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
@@ -8,8 +11,17 @@ public class LinkController {
     @PostMapping("/links")
     public LinkResponse shortLink(@RequestBody LinkRequest request){
         LinkResponse response=new LinkResponse();
-        response.setShortCode("abc123");
+        response.setShortCode(generateShortCode());
         response.setLongUrl(request.getLongUrl());
         return response;
+    }
+     public String generateShortCode(){
+        String pool = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+        String shortCodeStr="";
+       Random rand=new Random();
+        for(int i=0;i<6;i++){
+            shortCodeStr+=pool.charAt(rand.nextInt(pool.length()));
+        }
+        return shortCodeStr;
     }
 }
